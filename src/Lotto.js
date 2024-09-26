c39c2c59276b134a629f04af5dabb053cb389ddf
wo-o29 <woogur29@gmail.com>
2024-09-26 22:37:03 +0900

    refactor: 기존 코드 수정, 주석 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,20 @@
-class Lotto {
-  #numbers;
+import { MESSAGES } from "./constants";
 
+class Lotto {
+  #numbers; // 클래스 외부에서 접근 불가
+
+  // 객체가 생성될 때 호출
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
+  // 유효성 검사
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(MESSAGES.SIX_LENGTH_LOTTO_NUMBER);
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const MESSAGES = Object.freeze({
-  INVALID_LOTTO_PRICE: "[ERROR] 로또 가격은 1000원 단위로 입력해 주세요!",
-  SIX_LENGTH_LOTTO_NUMBER: "[ERROR] 쉼표를 기준으로 6개의 숫자만 입력 가능합니다.!",
+  INVALID_LOTTO_PRICE: "[ERROR] 로또 가격은 1000원 단위로 입력해 주세요.",
+  SIX_LENGTH_LOTTO_NUMBER: "[ERROR] 쉼표를 기준으로 6개의 숫자만 입력 가능합니다.",
   DUPLICATE_LOTTO_NUMBER: "[ERROR] 중복된 숫자가 있습니다.",
   INVALID_LOTTO_NUMBER: "[ERROR] 1부터 45까지의 숫자만 입력 가능합니다.",
   INPUT_LOTTO_PRICE: "구입금액을 입력해 주세요.",
