471e63b66c73948534f4d5110751c2b531947f50
wo-o29 <woogur29@gmail.com>
2024-10-02 23:13:42 +0900

    feat: 로또 프로그램 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Lotto from "./Lotto.js";
+
 class App {
-  async play() {}
+  async play() {
+    const lotto = new Lotto();
+    // 로또 구매 금액 입력
+    const price = await lotto.getUserInputLottoPrice();
+
+    // 로또 번호 생성, 출력
+    const lottoNumbersStore = lotto.getLottoNumbers(price);
+    lotto.printLottoNumbers(lottoNumbersStore);
+
+    // 당첨 번호 입력
+    const userLottoNumbers = await lotto.getUserInputLottoNumber();
+
+    // 보너스 번호 입력
+    const bonusNumber = await lotto.getUserInputBonusNumber();
+
+    // 로또 결과 처리, 출력
+    const { result, prizeMoney } = lotto.getLottoResult(userLottoNumbers, bonusNumber, lottoNumbersStore);
+    lotto.printResult(result, price, prizeMoney);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
