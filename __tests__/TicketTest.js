432bf1da08ced3e333ae7ad0eac501a29c06cebc
jiyeeeah <sije3757@snu.ac.kr>
2024-10-03 21:29:05 +0900

    [feat] TicketTest 추가
    - 구매 금액을 입력시 구입 금액에 관한 유효성 검사와
      금액에 따른 티켓 개수의 계산을 검증하기 위한 test 코드 작성

---
+++ __tests__/TicketTest.js
@@ -0,0 +1,26 @@
+import { ERROR_MESSAGES } from "../src/constants/message";
+import { LOTTO } from "../src/constants/rules";
+import Ticket from "../src/Model/Ticket";
+
+describe("티켓 클래스 테스트", () => {
+  test("구입 금액이 숫자여야 한다.", () => {
+    expect(() => {
+      new Ticket("hi");
+    }).toThrow(ERROR_MESSAGES.ONLY_NUMBER);
+  });
+
+  test("구입 금액이 단위에 나눠 떨어져야한다.", () => {
+    expect(() => {
+      new Ticket("500");
+    }).toThrow(ERROR_MESSAGES.NOT_RIGHT_UNIT(LOTTO.PURCHASE_UNIT));
+  });
+
+  const AMOUNT_TEST_CASE = [
+    ["5000", 5],
+    ["132000", 132],
+  ];
+  test.each(AMOUNT_TEST_CASE)("구입 금액에 따른 티켓 개수 계산 test", (input, amount) => {
+    const ticket = new Ticket(input);
+    expect(ticket.getAmount()).toEqual(amount);
+  });
+});
