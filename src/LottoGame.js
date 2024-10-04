e2b1befc2a4033089d1f7e19ac5a7b76e74d5e6a
DevTeddy123 <ab315601@naver.com>
2024-10-04 16:22:01 +0900

    [feat] 로또 랜덤 값 생성

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,7 +1,10 @@
 class LottoGame {
 	#lottoAmount //로또 개수
+
+	#lottoRandomNumberArr = [] //로또 랜덤값 배열 (이중 배열)
 	async start() {
 		await this.inputMoneyAndLottoAmount()
+		this.randomNumberArr()
 	}
 
 	//돈 입력 및 로또 구매 수 출력
@@ -15,6 +18,15 @@ class LottoGame {
 		MissionUtils.Console.print(`\n${this.#lottoAmount}개를 구매했습니다.`)
 	}
 
+	//랜덤한 값 배열로
+	randomNumberArr() {
+		for (let i = 0; i < this.#lottoAmount; i++) {
+			const randomNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+			randomNum.sort((a, b) => a - b)
+			this.#lottoRandomNumberArr.push(randomNum)
+			MissionUtils.Console.print(randomNum)
+		}
+	}
 	}
 }
 export default LottoGame
