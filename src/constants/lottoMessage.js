4236a69855e4d441bf4ee7be37827d9ce0fc9c4f
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-07 00:24:08 +0900

    refac: 개행 추가

--- src/constants/lottoMessage.js
+++ src/constants/lottoMessage.js
@@ -1,6 +1,6 @@
 export const INPUT_MESSAGE = Object.freeze({
   PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
-  WINNING_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+  WINNING_NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
 });
 
