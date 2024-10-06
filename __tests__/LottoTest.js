92b7b592f0156c05b3e49252ca0b1b257832294f
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-07 00:33:57 +0900

    feat: test코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,30 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생", () => {
+    expect(() => {
+      new Lotto([0, 1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호의 개수가 6개 미만이면 예외가 발생", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("유효한 로또 번호로 가능", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test("로또 번호에 숫자가 아닌 값이 포함되면 예외가 발생", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "a"]);
+    }).toThrow("[ERROR]");
+  });
 });
